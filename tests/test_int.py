56163a601ea75f7806d0640d931e218d70561d53
tom@nymphenburg <none@none>
2008-10-14 22:01:54 +0900

    * added one test of int comparing.
    --HG--
    extra : convert_revision : ea76b95c30dc73115b45a19da0bfa62372828a00

--- tests/test_int.py
+++ tests/test_int.py
@@ -7,7 +7,10 @@ class TestInt(TestCase):
     def test_plus_one(self):
         self._test("puts 42 + 1", "43\n")
 
-    def test_compare(self):
+    def test_compare1(self):
         self._test("puts 0 < 42", "true\n")
+
+    def test_compare2(self):
+        self._test("puts 42 < 0", "false\n")
 
 # vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
