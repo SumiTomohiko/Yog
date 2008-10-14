c43776c85646d731e1a4735eea5d7d00546641eb
tom@nymphenburg <none@none>
2008-10-14 18:39:29 +0900

    * added a test for assign.
    --HG--
    extra : convert_revision : a030b562005b77320b63e5585db848022758cfb6

---
+++ tests/test_assign.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from tests import TestCase
+
+class TestAssign(TestCase):
+
+    def test_assign(self):
+        self._test("""
+foo = 42
+puts foo""", "42\n")
+
+# vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
