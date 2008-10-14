36eeabd0f30ad34876af622c0eaf57c2afb4700b
tom@nymphenburg <none@none>
2008-10-14 22:06:28 +0900

    * added tests for while statement.
    --HG--
    extra : convert_revision : 6c8ac8d18ec7a75c01f432254f471c8d367f8836

---
+++ tests/test_while.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from tests import TestCase
+
+class TestWhile(TestCase):
+
+    def test_while1(self):
+        self._test("""
+i = 0
+while i < 1
+    puts i
+end""", """0
+""")
+
+    def test_while2(self):
+        self._test("""
+i = 0
+while i < 2
+    puts i
+end""", """0
+1
+""")
+
+    def test_while3(self):
+        self._test("""
+i = 0
+while i < 0
+    puts i
+end""", "")
+
+# vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
