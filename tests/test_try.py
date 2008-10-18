8e4259922740ba8fc0e97df3bc4b5584a4fb0ddb
tom@nymphenburg <none@none>
2008-10-18 14:50:11 +0900

    * added tests for break in try.
    --HG--
    extra : convert_revision : ad305d49e09552bca631f54f76d1df98953f46c5

--- tests/test_try.py
+++ tests/test_try.py
@@ -12,4 +12,34 @@ except
     puts 42
 end""", "42\n")
 
+    def test_break_in_finally1(self):
+        self._test("""
+i = 0
+while i < 10
+    try
+        break
+    finally
+        puts 42
+    end
+end
+""", "42\n")
+
+    def test_break_in_finally2(self):
+        self._test("""
+i = 0
+while i < 10
+    try
+        try
+            break
+        finally
+            puts 42
+        end
+    finally
+        puts 43
+    end
+end
+""", """42
+43
+""")
+
 # vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
