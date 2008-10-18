0758565f9b7cdc90666d1accab5db96d994bdb39
tom@nymphenburg <none@none>
2008-10-18 12:10:21 +0900

    * added tests for is statement.
    --HG--
    extra : convert_revision : 9807678a6f4cec7e878259be199378694708ad77

---
+++ tests/test_if.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from tests import TestCase
+
+class TestIf(TestCase):
+
+    def test_true(self):
+        self._test("""
+if 0 < 1
+    puts 42
+else
+    puts 0
+end
+""", "42\n")
+
+    def test_false(self):
+        self._test("""
+if 1 < 0
+    puts 0
+else
+    puts 42
+end
+""", "42\n")
+
+# vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
