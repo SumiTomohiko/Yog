ed8cfbe3f879d97f0d06759950e286a50f2553a4
tom@nymphenburg <none@none>
2008-11-04 19:42:07 +0900

    * added a test for Int#times.
    --HG--
    extra : convert_revision : b91719091476e36052bbbf0de26511f253d27488

--- tests/test_int.py
+++ tests/test_int.py
@@ -13,4 +13,21 @@ class TestInt(TestCase):
     def test_compare2(self):
         self._test("puts 42 < 0", "false\n")
 
+    def test_times(self):
+        self._test("""
+10.times() { (n) 
+    puts n
+}
+""", """0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+""")
+
 # vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
