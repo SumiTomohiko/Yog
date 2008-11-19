ac976e3ad4adf7e361fbb9fb064262941365123a
tom@nymphenburg <none@none>
2008-11-19 13:00:54 +0900

    * added tests for __LINE__.
    --HG--
    extra : convert_revision : 885b17299d50b650ad9f34877afd50da202c36d3

---
+++ tests/test_line.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from tests import TestCase
+
+class TestLine(TestCase):
+
+    def test_line1(self):
+        self._test("puts __LINE__", """1
+""")
+
+    def test_line2(self):
+        self._test("""
+# comment
+puts __LINE__
+""", """3
+""")
+
+# vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
