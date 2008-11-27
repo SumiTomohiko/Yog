85707ed9eb3151da8564ea3b09f06fc845ebf308
tom@nymphenburg <none@none>
2008-11-27 16:44:36 +0900

    * tests/test_return.py: added tests for return statement.
    --HG--
    extra : convert_revision : 7055bb1603dd7a19c571fb059f933d38cbe7b149

---
+++ tests/test_return.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from tests import TestCase
+
+class TestReturn(TestCase): 
+
+    def test_return1(self):
+        self._test("""
+def foo()
+  return 42
+end
+
+puts foo()
+""", """42
+""")
+
+    def test_return2(self):
+        self._test("""
+def foo()
+  return 42
+end
+
+puts foo()
+puts 43
+""", """42
+43
+""")
+
+# vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
