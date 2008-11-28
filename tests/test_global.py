4369a9f3b3f4afda7bc85a623c2db23a4895f2b8
tom@nymphenburg <none@none>
2008-11-28 20:55:57 +0900

    * tests/test_global.py: test cases for global.
    --HG--
    extra : convert_revision : 6ad6417674f33b7b187265332585d388f75a5bb4

---
+++ tests/test_global.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from tests import TestCase
+
+class TestGlobal(TestCase):
+
+    def test_global1(self):
+        self._test("""
+def foo()
+  puts 42
+end
+
+def bar()
+  foo()
+end
+
+bar()""", """42
+""")
+
+    def test_global2(self):
+        self._test("""
+foo = 42
+
+def bar()
+  puts foo
+end
+
+bar()""", """42
+""")
+
+    def test_global3(self):
+        self._test("""
+foo = 42
+
+def bar()
+  nonlocal foo
+  foo = 43
+end
+
+bar()
+
+puts foo""", """43
+""")
+
+# vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
