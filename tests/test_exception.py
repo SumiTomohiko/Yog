b44f26900ee7bdc850601a3b8e8547af7db92137
tom@nymphenburg <none@none>
2008-11-30 17:12:39 +0900

    * tests/test_exception.py (TestException#test_traceback2): added a test for traceback of a function.
    --HG--
    extra : convert_revision : 209e165bd2d7c260064d028abd15e2d2744f8edf

--- tests/test_exception.py
+++ tests/test_exception.py
@@ -5,7 +5,7 @@ from tests import TestCase
 
 class TestException(TestCase):
 
-    def test_traceback(self):
+    def test_traceback1(self):
         def test_stderr(stderr):
             m = match(r"""Traceback \(most recent call last\):
   File "[^"]+", line 1, in <module>
@@ -14,5 +14,21 @@ Exception: nil
             assert m is not None
 
         self._test("raise Exception.new()", stderr=test_stderr)
+
+    def test_traceback2(self):
+        def test_stderr(stderr):
+            m = match(r"""Traceback \(most recent call last\):
+  File "[^"]+", line 6, in <module>
+  File "[^"]+", line 3, in foo
+Exception: nil
+""", stderr)
+            assert m is not None
+
+        self._test("""
+def foo()
+  raise Exception.new()
+end
+
+foo()""", stderr=test_stderr)
 
 # vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
