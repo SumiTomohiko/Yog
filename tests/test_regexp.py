a92e81d3cd6fe6e52074aaf258a06ecbff6b984d
tom@nymphenburg <none@none>
2008-12-02 16:14:18 +0900

    * tests/test_regexp.py: added tests for Match object.
    --HG--
    extra : convert_revision : da1b0a207788645f6efc0a91d23a7383a6f8d47f

--- tests/test_regexp.py
+++ tests/test_regexp.py
@@ -21,7 +21,7 @@ s =~ /foo/i""")
 
     def test_match_expr10(self):
         self._test("""
-s = "foo"
+s = \"foo\"
 if s =~ /foo/
   puts 42
 end""", """42
@@ -29,7 +29,7 @@ end""", """42
 
     def test_match_expr20(self):
         self._test("""
-s = "foo"
+s = \"foo\"
 if s =~ /bar/
 else
   puts 42
@@ -38,10 +38,82 @@ end""", """42
 
     def test_match_ignore_case10(self):
         self._test("""
-s = "foo"
+s = \"foo\"
 if s =~ /FOO/i
   puts 42
 end""", """42
+""")
+
+    def test_match_group10(self):
+        self._test("""
+m = \"foo\" =~ /foo/
+puts m.group(0)""", """foo
+""")
+
+    def test_match_group20(self):
+        self._test("""
+m = \"foobarbaz\" =~ /bar/
+puts m.group(0)""", """bar
+""")
+
+    def test_match_group30(self):
+        self._test("""
+m = \"foobarbazquux\" =~ /foo(bar)baz/
+puts m.group(1)""", """bar
+""")
+
+    def test_match_group40(self):
+        self._test("""
+m = \"foobarbazquux\" =~ /foo(?<name>bar)baz/
+puts m.group(\"name\")""", """bar
+""")
+
+    def test_match_start10(self):
+        self._test("""
+m = \"foo\" =~ /foo/
+puts m.start(0)""", """0
+""")
+
+    def test_match_start20(self):
+        self._test("""
+m = \"foobarbaz\" =~ /bar/
+puts m.start(0)""", """3
+""")
+
+    def test_match_start30(self):
+        self._test("""
+m = \"foobarbazquux\" =~ /foo(bar)baz/
+puts m.start(1)""", """3
+""")
+
+    def test_match_start40(self):
+        self._test("""
+m = \"foobarbazquux\" =~ /foo(?<name>bar)baz/
+puts m.start(\"name\")""", """3
+""")
+
+    def test_match_end10(self):
+        self._test("""
+m = \"foo\" =~ /foo/
+puts m.end(0)""", """3
+""")
+
+    def test_match_end20(self):
+        self._test("""
+m = \"foobarbaz\" =~ /bar/
+puts m.end(0)""", """6
+""")
+
+    def test_match_end30(self):
+        self._test("""
+m = \"foobarbazquux\" =~ /foo(bar)baz/
+puts m.end(1)""", """6
+""")
+
+    def test_match_end40(self):
+        self._test("""
+m = \"foobarbazquux\" =~ /foo(?<name>bar)baz/
+puts m.end(\"name\")""", """6
 """)
 
 # vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
