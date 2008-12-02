a85dcc025b413a1638b52fac9638452b9263011d
tom@nymphenburg <none@none>
2008-12-02 22:59:01 +0900

    * tools/hg/preoutgoing.py (hook): changed to commit.
    --HG--
    extra : convert_revision : 6357b052f451919ea87da1bfef2862e41d9ad66c

--- tools/hg/preoutgoing.py
+++ tools/hg/preoutgoing.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
 def hook(ui, repo, **kwargs):
-    print ui
-    print repo
-    print kwargs
+    print ui, repo, kwargs
 
 # vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
