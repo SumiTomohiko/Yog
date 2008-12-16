edcc374f1b25e48d3030628ec503774aecd813c1
tom@nymphenburg <none@none>
2008-12-16 18:50:30 +0900

    * src/bool.c (YogBool_klass_new): changed to update pointer.
    --HG--
    extra : convert_revision : 8dfdf0bf2b7f2853b62bb9523b315c33bc6757d9

--- src/bool.c
+++ src/bool.c
@@ -29,9 +29,15 @@ YogKlass*
 YogBool_klass_new(YogEnv* env) 
 {
     YogKlass* klass = YogKlass_new(env, "Bool", ENV_VM(env)->cObject);
+    FRAME_DECL_LOCAL(env, klass_idx, OBJ2VAL(klass));
+
+#define UPDATE_PTR  FRAME_LOCAL_OBJ(env, klass, YogKlass, klass_idx)
+    UPDATE_PTR;
     YogKlass_define_method(env, klass, "to_s", to_s, 0, 0, 0, 0, NULL);
 
+    UPDATE_PTR;
     return klass;
+#undef UPDATE_PTR
 }
 
 /**
