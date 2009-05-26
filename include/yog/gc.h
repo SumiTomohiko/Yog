baa629384a7fdcc380e791f729747624ac3e6495
tom@nymphenburg <none@none>
2009-05-26 13:48:21 +0900

    チェンジセット cdc6db43a921 の打ち消し
    --HG--
    extra : convert_revision : e3e714290f2784c58f9b61b756e6978765d1bfb0

--- include/yog/gc.h
+++ include/yog/gc.h
@@ -31,7 +31,6 @@
 
 /* src/gc.c */
 YogVal YogGC_allocate(YogEnv*, ChildrenKeeper, Finalizer, size_t);
-void YogGC_initialize(YogEnv*);
 void YogGC_keep(YogEnv*, YogVal*, ObjectKeeper, void*);
 void YogGC_perform(YogEnv*);
 void YogGC_perform_major(YogEnv*);
