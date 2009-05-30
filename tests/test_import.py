1120a61543e8839803ed792aeddc7c943295cc26
tom@nymphenburg <none@none>
2009-05-30 11:18:54 +0900

    Added a test for importing plural packages.
    --HG--
    rename : tests/test_package.yg => tests/test_package1.yg
    
    --HG--
    extra : convert_revision : 7a75c753b4150761c688ff23f64527c2c8d5b9c2

--- tests/test_import.py
+++ tests/test_import.py
@@ -5,6 +5,9 @@ from testcase import TestCase
 class TestImport(TestCase):
 
     def test_import1(self):
-        self._test("import test_package", "42\n")
+        self._test("import test_package1", "42\n")
+
+    def test_import2(self):
+        self._test("import test_package1, test_package2", "42\n26\n")
 
 # vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
