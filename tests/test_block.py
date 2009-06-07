43a894a74eaf3646ef6a0e0c8bd08c4a07513f35
tom@nymphenburg <none@none>
2009-06-07 20:03:40 +0900

    Added tests for scope of variables in blocks. refs 46.
    --HG--
    extra : convert_revision : d6dc45e31adea3686fb16a6bfedb0c2ba1439e0c

--- tests/test_block.py
+++ tests/test_block.py
@@ -20,4 +20,113 @@ class TestBlock(TestCase):
         self._test("1.times() do puts 42 end", """42
 """)
 
+    def test_block5(self):
+        self._test("""
+1.times() do [n]
+  m = n
+end
+
+puts m""", """0
+""")
+
+    def test_block6(self):
+        self._test("""
+1.times() do [n]
+  1.times() do [m]
+    l = m
+  end
+end
+
+puts l""", """0
+""")
+
+    def test_block7(self):
+        self._test("""
+n = 42
+1.times() do [n]
+  puts n
+end
+
+puts n""", """0
+42
+""")
+
+    def test_block8(self):
+        self._test("""
+def main()
+  1.times() do [n]
+    m = n
+  end
+
+  puts m
+end
+
+main()""", """0
+""")
+
+    def test_block9(self):
+        self._test("""
+def main()
+  1.times() do [n]
+    1.times() do [m]
+      l = m
+    end
+  end
+
+  puts l
+end
+
+main()""", """0
+""")
+
+    def test_block10(self):
+        self._test("""
+def main()
+  n = 42
+  1.times() do [n]
+    puts n
+  end
+  puts n
+end
+
+main()""", """0
+42
+""")
+
+    def test_block11(self):
+        self._test("""
+class Foo
+  1.times() do [n]
+    m = n
+  end
+  puts m
+end
+""", """0
+""")
+
+    def test_block12(self):
+        self._test("""
+class Foo
+  1.times() do [n]
+    1.times() do [m]
+      l = m
+    end
+  end
+  puts l
+end
+""", """0
+""")
+
+    def test_block13(self):
+        self._test("""
+class Foo
+  n = 42
+  1.times() [n]
+    puts n
+  end
+  puts n
+end""", """0
+42
+""")
+
 # vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
