af990318a817fa1d93bc23db658ed60e99eb6737
tom@nymphenburg <none@none>
2009-07-16 20:11:41 +0900

    added include/yog/version.h. refs 53.
    --HG--
    extra : convert_revision : 773d8b86ef1604fdfbc63093114ad2e501c7d4d3

---
+++ include/yog/version.h
@@ -0,0 +1,11 @@
+
+
+#if !defined(__YOG_VERSION_H__)
+#define __YOG_VERSION_H__
+
+#define PACKAGE_REVISION    "638 0c165a94be7e"
+
+#endif
+/**
+ * vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
+ */
