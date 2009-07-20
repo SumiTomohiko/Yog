e01103a886f93c861e66b9f9ea304a07befd51fb
tom@nymphenburg <none@none>
2009-07-20 20:31:35 +0900

    added interactive tests. refs 127.
    --HG--
    extra : convert_revision : 7e711df7e5b3b7ae28377e971533ff8cbb9f9a22

---
+++ tests/test_interactive.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from testcase import TestCase
+
+class TestInteractive(TestCase):
+
+    def test_interactive0(self):
+        self._test(stdout=""">>>
+42
+""", stdin="""puts(42)
+""")
+
+    def test_interactive10(self):
+        self._test(stdout=""">>>
+42
+>>>
+26
+""", stdin="""puts(42)
+puts(26)
+""")
+
+# vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
--- tests/testcase.py
+++ tests/testcase.py
@@ -18,7 +18,7 @@ class TestCase(object):
             t.append(line)
         return "\n".join(t)
 
-    def _test(self, src, stdout="", stderr="", status=0, options=[], timeout=120, remove_tmpfile=True):
+    def get_command(self):
         try:
             env_gc = environ["GC"]
         except KeyError:
@@ -33,51 +33,70 @@ class TestCase(object):
             cmd_name = "yog-bdw"
         elif env_gc == "generational":
             cmd_name = "yog-generational"
+        return cmd_name
 
-        options = options or ["--gc-stress"]
+    def do(self, stdout, stderr, stdin, status, args, timeout):
+        cmd = [join("..", "src", self.get_command())] + args
 
+        proc = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        if stdin is not None:
+            proc.stdin.write(stdin)
+        proc.stdin.close()
+
+        time_begin = time()
+        while True:
+            if proc.poll() is not None:
+                break
+            if timeout < time() - time_begin:
+                kill(proc.pid, SIGKILL)
+                assert False, "time is out."
+
+        if stderr is not None:
+            err = self.remove_gc_warings(proc.stderr.read())
+            if callable(stderr):
+                stderr(err)
+            else:
+                assert stderr == err
+
+        if stdout is not None:
+            out = proc.stdout.read()
+            if callable(stdout):
+                stdout(out)
+            else:
+                assert stdout == out
+
+        returncode = proc.returncode
+        if callable(status):
+            status(returncode)
+        else:
+            assert status == returncode
+
+    def write_source(self, path, src):
+        f = open(path, "w")
+        try:
+            f.write(src)
+        finally:
+            f.close()
+
+    def _test_source(self, src, stdout, stderr, stdin, status, options, timeout, remove_tmpfile=True):
         file = mkstemp(prefix="yog", suffix=".yg")[1]
         try:
-            f = open(file, "w")
-            try:
-                f.write(src)
-            finally:
-                f.close()
-
-            cmd = [join("..", "src", cmd_name)]
-            cmd.extend(options)
-            cmd.append(file)
-
-            proc = Popen(cmd, stdout=PIPE, stderr=PIPE)
-            time_begin = time()
-            while True:
-                if proc.poll() is not None:
-                    break
-                if timeout < time() - time_begin:
-                    kill(proc.pid, SIGKILL)
-                    assert False, "time is out."
-
-            if stderr is not None:
-                err = self.remove_gc_warings(proc.stderr.read())
-                if callable(stderr):
-                    stderr(err)
-                else:
-                    assert stderr == err
-
-            if stdout is not None:
-                out = proc.stdout.read()
-                if callable(stdout):
-                    stdout(out)
-                else:
-                    assert stdout == out
-
-            returncode = proc.returncode
-            if callable(status):
-                status(returncode)
-            else:
-                assert status == returncode
+            self.write_source(file, src)
+            args = options + [file]
+            self.do(stdout, stderr, stdin, status, args, timeout)
         finally:
             if remove_tmpfile:
                 unlink(file)
+
+    def _test_interactive(self, stdout, stderr, stdin, status, options, timeout):
+        self.do(stdout, stderr, stdin, status, options, timeout)
+
+    def _test(self, src=None, stdout="", stderr="", stdin=None, status=0, options=[], timeout=120, remove_tmpfile=True):
+        options = options or ["--gc-stress"]
+
+        if src is not None:
+            self._test_source(src, stdout, stderr, stdin, status, options, timeout, remove_tmpfile)
+        else:
+            self._test_interactive(stdout, stderr, stdin, status, options, timeout)
 
 # vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
