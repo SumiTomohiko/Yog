35dfbeff9083fff56569c64c07bc00f0fb424e42
tom@nymphenburg <none@none>
2009-07-21 00:39:18 +0900

    added a test for symbol. refs 123.
    --HG--
    extra : convert_revision : 8c0d7d741ffd82a9f10baa479e735a1e57de62ca

---
+++ tests/test_symbol.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from testcase import TestCase
+
+class TestSymbol(TestCase):
+
+    def test_symbol0(self):
+        self._test("""
+puts(:foo)
+""", """foo
+""")
+
+# vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
