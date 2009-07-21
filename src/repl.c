9cc67156811a65595172775ecaf808fb67eb42a9
tom@nymphenburg <none@none>
2009-07-21 16:03:15 +0900

    changed to handle exceptions in repl. closes 129.
    --HG--
    extra : convert_revision : 44755183040fb2cab9ca439aefbf9bd730491ad1

--- src/repl.c
+++ src/repl.c
@@ -1,6 +1,8 @@
+#include <setjmp.h>
 #include <stdio.h>
 #include "yog/compile.h"
 #include "yog/encoding.h"
+#include "yog/error.h"
 #include "yog/eval.h"
 #include "yog/package.h"
 #include "yog/parser.h"
@@ -15,16 +17,51 @@ print_prompt()
     printf(">>> ");
 }
 
+static void
+eval(YogEnv* env, YogVal pkg, YogVal src)
+{
+    SAVE_ARGS2(env, pkg, src);
+    YogVal stmts = YUNDEF;
+    YogVal code = YUNDEF;
+    YogVal cur_frame = YUNDEF;
+    PUSH_LOCALS3(env, stmts, code, cur_frame);
+    YogLocals* locals = PTR_AS(YogThread, env->thread)->locals;
+    cur_frame = PTR_AS(YogThread, env->thread)->cur_frame;
+
+    YogJmpBuf jmpbuf;
+    int status = 0;
+    if ((status = setjmp(jmpbuf.buf)) == 0) {
+        PUSH_JMPBUF(env->thread, jmpbuf);
+
+        stmts = YogParser_parse(env, src);
+        if (!IS_PTR(stmts)) {
+            RETURN_VOID(env);
+        }
+        code = YogCompiler_compile_module(env, MAIN_MODULE_NAME, stmts);
+
+        PTR_AS(YogPackage, pkg)->code = code;
+        YogEval_eval_package(env, pkg);
+    }
+    else {
+        PTR_AS(YogThread, env->thread)->locals = locals;
+        PTR_AS(YogThread, env->thread)->cur_frame = cur_frame;
+        YogError_print_stacktrace(env);
+    }
+
+    YogString_clear(env, src);
+    print_prompt();
+
+    RETURN_VOID(env);
+}
+
 void
 YogRepl_do(YogEnv* env)
 {
     SAVE_LOCALS(env);
-    YogVal stmts = YUNDEF;
-    YogVal code = YUNDEF;
     YogVal pkg = YUNDEF;
     YogVal src = YUNDEF;
     YogVal enc = YUNDEF;
-    PUSH_LOCALS5(env, stmts, code, pkg, src, enc);
+    PUSH_LOCALS3(env, pkg, src, enc);
 
     pkg = YogPackage_new(env);
     YogVM_register_package(env, env->vm, MAIN_MODULE_NAME, pkg);
@@ -38,18 +75,7 @@ YogRepl_do(YogEnv* env)
     char buffer[4096];
     while (fgets(buffer, array_sizeof(buffer), stdin) != NULL) {
         YogString_add(env, src, buffer);
-
-        stmts = YogParser_parse(env, src);
-        if (!IS_PTR(stmts)) {
-            continue;
-        }
-        code = YogCompiler_compile_module(env, MAIN_MODULE_NAME, stmts);
-
-        PTR_AS(YogPackage, pkg)->code = code;
-        YogEval_eval_package(env, pkg);
-
-        YogString_clear(env, src);
-        print_prompt();
+        eval(env, pkg, src);
     }
 
     RETURN_VOID(env);
--- tests/test_interactive.py
+++ tests/test_interactive.py
@@ -16,4 +16,27 @@ class TestInteractive(TestCase):
 puts(26)
 """)
 
+    def test_interactive20(self):
+        self._test(stdout=""">>> foobar
+>>> >>> """, stderr="""Traceback (most recent call last):
+  File \"__main__\", in <module>
+  File builtin, in String#+
+TypeError: can't convert 'Int' object to string implicitly
+""", stdin="""puts(\"foo\" + \"bar\")
+puts(\"foo\" + 42)
+""")
+
+    def test_interactive30(self):
+        self._test(stdout=">>> >>> >>> ", stderr="""Traceback (most recent call last):
+  File \"__main__\", in <module>
+  File builtin, in String#+
+TypeError: can't convert 'Int' object to string implicitly
+Traceback (most recent call last):
+  File \"__main__\", in <module>
+  File builtin, in String#+
+TypeError: can't convert 'Int' object to string implicitly
+""", stdin="""puts(\"foo\" + 42)
+puts(\"foo\" + 42)
+""")
+
 # vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
