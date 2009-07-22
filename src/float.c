4b87c2d5fd73ae4cdbbd0c85abcd81f0c31d7d64
tom@nymphenburg <none@none>
2009-07-22 23:57:52 +0900

    implemented Float#-. closes 137.
    --HG--
    extra : convert_revision : 0c7bb3ab85e1fb64785b2defd68f44cb21e6f637

--- src/float.c
+++ src/float.c
@@ -90,6 +90,40 @@ add(YogEnv* env, YogVal self, YogVal args, YogVal kw, YogVal block)
     RETURN(env, YUNDEF);
 }
 
+static YogVal
+subtract(YogEnv* env, YogVal self, YogVal args, YogVal kw, YogVal block)
+{
+    SAVE_ARGS4(env, self, args, kw, block);
+    YogVal right = YUNDEF;
+    YogVal result = YUNDEF;
+    PUSH_LOCALS2(env, right, result);
+
+    right = YogArray_at(env, args, 0);
+    YOG_ASSERT(env, !IS_UNDEF(right), "right is undef");
+    if (IS_INT(right)) {
+        result = YogFloat_new(env);
+        FLOAT_NUM(result) = FLOAT_NUM(self) - VAL2INT(right);
+        RETURN(env, result);
+    }
+    else if (IS_NIL(right) || IS_BOOL(right) || IS_SYMBOL(right)) {
+    }
+    else if (IS_OBJ_OF(env, right, cBignum)) {
+        result = YogFloat_new(env);
+        FLOAT_NUM(result) = FLOAT_NUM(self) - mpz_get_d(BIGNUM_NUM(right));
+        RETURN(env, result);
+    }
+    else if (IS_OBJ_OF(env, right, cFloat)) {
+        result = YogFloat_new(env);
+        FLOAT_NUM(result) = FLOAT_NUM(self) - FLOAT_NUM(right);
+        RETURN(env, result);
+    }
+
+    YogError_raise_binop_type_error(env, self, right, "-");
+
+    /* NOTREACHED */
+    RETURN(env, YUNDEF);
+}
+
 YogVal 
 YogFloat_klass_new(YogEnv* env) 
 {
@@ -103,6 +137,7 @@ YogFloat_klass_new(YogEnv* env)
     YogKlass_define_method(env, klass, "-self", negative);
     YogKlass_define_method(env, klass, "to_s", to_s);
     YogKlass_define_method(env, klass, "+", add);
+    YogKlass_define_method(env, klass, "-", subtract);
 
     RETURN(env, klass);
 }
