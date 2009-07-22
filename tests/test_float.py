2213e79add8ad33afd993e796766fe37c51b4e88
tom@nymphenburg <none@none>
2009-07-22 22:37:43 +0900

    added tests for Float#-. refs 137.
    --HG--
    extra : convert_revision : 67466a96f6e596db6da1e8ca5f64bdab5d0d7861

--- tests/test_float.py
+++ tests/test_float.py
@@ -80,4 +80,67 @@ TypeError: unsupported operand type\(s\) for \+: 'Float' and 'Symbol'
 puts(3.1415926535 + :foo)
 """, stderr=test_stderr)
 
+    def test_subtract0(self):
+        self._test("""
+# Float - int
+puts(3.141592 - 42)
+""", """-38.8584
+""")
+
+    def test_subtract10(self):
+        self._test("""
+# Float - Bignum
+puts(3.141592 - 4611686018427387904)
+""", """-4.61169e+18
+""")
+
+    def test_subtract20(self):
+        self._test("""
+# Float - Float
+puts(3.141592 + 2.71828183)
+""", """0.42331
+""")
+
+    def test_subtract30(self):
+        def test_stderr(stderr):
+            m = match(r"""Traceback \(most recent call last\):
+  File "[^"]+", line 3, in <module>
+  File builtin, in Float#-
+TypeError: unsupported operand type\(s\) for -: 'Float' and 'Bool'
+""", stderr)
+            assert m is not None
+
+        self._test("""
+# Float - Bool (TypeError)
+puts(3.1415926535 - true)
+""", stderr=test_stderr)
+
+    def test_subtract40(self):
+        def test_stderr(stderr):
+            m = match(r"""Traceback \(most recent call last\):
+  File "[^"]+", line 3, in <module>
+  File builtin, in Float#-
+TypeError: unsupported operand type\(s\) for -: 'Float' and 'Nil'
+""", stderr)
+            assert m is not None
+
+        self._test("""
+# Float - nil (TypeError)
+puts(3.1415926535 - nil)
+""", stderr=test_stderr)
+
+    def test_subtract50(self):
+        def test_stderr(stderr):
+            m = match(r"""Traceback \(most recent call last\):
+  File "[^"]+", line 3, in <module>
+  File builtin, in Float#-
+TypeError: unsupported operand type\(s\) for -: 'Float' and 'Symbol'
+""", stderr)
+            assert m is not None
+
+        self._test("""
+# Float - Symbol (TypeError)
+puts(3.1415926535 - :foo)
+""", stderr=test_stderr)
+
 # vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
