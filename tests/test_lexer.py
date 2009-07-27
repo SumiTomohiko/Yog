34a8f24a2f4bd3bc4f180ce2e1779aa9a42b34c7
tom@nymphenburg <none@none>
2009-07-27 17:34:59 +0900

    added a test for invalid multibyte char. refs 88.
    --HG--
    extra : convert_revision : 63226f07fe4cfd7e5e375244a3ad741a5fc11077

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -23,4 +23,13 @@ class TestLexer(TestCase):
         self._test("""
 \"\\""", stderr=test_stderr)
 
+    def test_string10(self):
+        def test_stderr(stderr):
+            m = match(r"""SyntaxError: invalid multibyte char
+""", stderr)
+            assert m is not None
+
+        self._test("""
+\"\xe7""", stderr=test_stderr)
+
 # vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
