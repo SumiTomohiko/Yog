13f2be6b4d92a74acb05db12fe9581dde7525d0b
tom@nymphenburg <none@none>
2009-07-28 15:16:38 +0900

    added a test for parser error. refs 82.
    --HG--
    extra : convert_revision : e4c73a9dda733648f88d89cede3094d7b217e1bf

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -15,4 +15,14 @@ def foo(bar, bar)
 end
 """, stderr=test_stderr)
 
+    def test_SyntaxError0(self):
+        def test_stderr(stderr):
+            m = match(r"""SyntaxError: file "[^"]+", line 2: invalid syntax
+""", stderr)
+            assert m is not None
+
+        self._test("""
+def def
+""", stderr=test_stderr)
+
 # vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
