cf851614b4c61ab4ba6ee1d002b894e63056f881
tom@nymphenburg <none@none>
2009-07-30 19:00:42 +0900

    added tests for property. refs 157.
    --HG--
    extra : convert_revision : 626fa485f6e57fe13699476c12ae824bb54eeef4

---
+++ tests/test_property.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+from re import match
+from testcase import TestCase
+
+class TestProperty(TestCase):
+
+    def test_property0(self):
+        def test_stdout(stdout):
+            m = match(r"""<Class [0-9a-fA-Z]+>
+""", stdout)
+            assert m is not None
+
+        self._test("""
+o = Object.new()
+puts(o.class)
+""", stdout=test_stdout)
+
+    def test_property10(self):
+        self._test("""
+class Foo
+  def get_bar()
+    puts(42)
+  end
+
+  bar = property(get_bar)
+end
+
+foo = Foo.new()
+puts(foo.bar)
+""", """42
+""")
+
+    def test_property20(self):
+        self._test("""
+class Foo
+  def get_bar()
+    return 42
+  end
+
+  bar = property(get_bar)
+end
+
+foo = Foo.new()
+puts(foo.bar)
+""", """42
+""")
+
+    def test_property30(self):
+        self._test("""
+class Foo
+  def set_bar(bar)
+    puts(42)
+  end
+
+  bar = property(nil, set_bar)
+end
+
+foo = Foo.new()
+foo.bar = 26
+""", """42
+""")
+
+    def test_property40(self):
+        self._test("""
+class Foo
+  def set_bar(bar)
+    self.bar = bar
+  end
+
+  bar = property(nil, set_bar)
+end
+
+foo = Foo.new()
+foo.bar = 42
+puts(foo.bar)
+""", """42
+""")
+
+# vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
