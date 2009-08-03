fc05d2245d4adbb88296db710c31920a73054e5b
tom@nymphenburg <none@none>
2009-08-03 12:57:53 +0900

    implemented ClassMethod descriptor. closes 158.
    --HG--
    extra : convert_revision : d68605e488a3437bef92ecc48577cb8e934b0290

--- src/classmethod.c
+++ src/classmethod.c
@@ -1,9 +1,43 @@
 #include "yog/classmethod.h"
+#include "yog/error.h"
+#include "yog/frame.h"
 #include "yog/gc.h"
 #include "yog/klass.h"
 #include "yog/object.h"
 #include "yog/thread.h"
 #include "yog/yog.h"
+
+static YogVal
+call_descr_get(YogEnv* env, YogVal attr, YogVal obj, YogVal klass)
+{
+    SAVE_ARGS3(env, attr, obj, klass);
+    YogVal method = YUNDEF;
+    YogVal f = YUNDEF;
+    YogVal klass_of_f = YUNDEF;
+    YogVal klass_of_klass = YUNDEF;
+    PUSH_LOCALS4(env, method, f, klass_of_f, klass_of_klass);
+
+    f = PTR_AS(YogClassMethod, attr)->f;
+    klass_of_f = YogVal_get_klass(env, f);
+    YOG_ASSERT(env, PTR_AS(YogKlass, klass_of_f)->call_get_descr != NULL, "f is not callable");
+    klass_of_klass = YogVal_get_klass(env, klass);
+    method = PTR_AS(YogKlass, klass_of_f)->call_get_descr(env, f, klass, klass_of_klass);
+
+    RETURN(env, method);
+}
+
+static void
+exec_descr_get(YogEnv* env, YogVal attr, YogVal obj, YogVal klass)
+{
+    SAVE_ARGS3(env, attr, obj, klass);
+    YogVal method = YUNDEF;
+    PUSH_LOCAL(env, method);
+
+    method = call_descr_get(env, attr, obj, klass);
+    FRAME_PUSH(env, method);
+
+    RETURN_VOID(env);
+}
 
 YogVal
 YogClassMethod_klass_new(YogEnv* env)
@@ -13,6 +47,8 @@ YogClassMethod_klass_new(YogEnv* env)
     PUSH_LOCAL(env, klass);
 
     klass = YogKlass_new(env, "ClassMethod", env->vm->cClassMethod);
+    YogKlass_define_descr_get_executor(env, klass, exec_descr_get);
+    YogKlass_define_descr_get_caller(env, klass, call_descr_get);
 
     RETURN(env, klass);
 }
