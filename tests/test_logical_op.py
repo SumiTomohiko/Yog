c6bacd96c88af472970f412a86cae9e78ed9d3fc
tom@nymphenburg <none@none>
2009-08-07 01:10:39 +0900

    added tests for || operator. refs 175.
    --HG--
    extra : convert_revision : 2634d90b6ca04461765ef9b4d74893e64ad221fe

--- tests/test_logical_op.py
+++ tests/test_logical_op.py
@@ -34,4 +34,34 @@ foo() && bar()
 """, """42
 """)
 
+    def test_or0(self):
+        self._test("""
+puts(42 || 26)
+""", """42
+""")
+
+    def test_or10(self):
+        self._test("""
+puts(false || 42)
+""", """42
+""")
+
+    def test_or20(self):
+        self._test("""
+# short circuit
+
+def foo()
+  puts(42)
+  return true
+end
+
+def bar()
+  puts(26)
+  return false
+end
+
+foo() || bar()
+""", """42
+""")
+
 # vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
