2764d0ebe6af6948b398aab62f1ed353d104a6ac
tom@nymphenburg <none@none>
2009-08-11 11:56:15 +0900

    added tests for variable parameter. refs 238.
    --HG--
    extra : convert_revision : 9373c06931b347cf6777ec2ef1d3dd7db66c1039

--- tests/test_argument.py
+++ tests/test_argument.py
@@ -124,4 +124,70 @@ foo(baz: 42)
 """, """42
 """)
 
+    def test_variable_parameter0(self):
+        self._test("""
+def foo()
+  puts(42)
+end
+
+bar = []
+foo(*bar)
+""", """42
+""")
+
+    def test_variable_parameter10(self):
+        self._test("""
+def foo(bar)
+  puts(bar)
+end
+
+baz = [42]
+foo(*baz)
+""", """42
+""")
+
+    def test_variable_parameter20(self):
+        self._test("""
+def foo(bar, baz)
+  puts(bar)
+end
+
+quux = [42, 26]
+foo(*quux)
+""", """42
+""")
+
+    def test_variable_parameter30(self):
+        self._test("""
+def foo(bar, baz)
+  puts(baz)
+end
+
+quux = [42, 26]
+foo(*quux)
+""", """26
+""")
+
+    def test_variable_parameter40(self):
+        self._test("""
+def foo(bar, baz)
+  puts(bar)
+end
+
+quux = [42]
+foo(26, *quux)
+""", """26
+""")
+
+    def test_variable_parameter50(self):
+        self._test("""
+def foo(bar, baz)
+  puts(baz)
+end
+
+quux = [42]
+foo(26, *quux)
+""", """42
+""")
+
 # vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
