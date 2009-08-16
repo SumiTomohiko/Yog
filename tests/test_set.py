ade71a6aa72da337165a6db7395fddc3f99adf15
SumiTomohiko <SumiTomohiko@neko-daisuki.ddo.jp>
2009-08-17 08:56:18 +0900

    add tests for Set class.
    --HG--
    extra : convert_revision : f868daf086c2d7d406f34c06fc164c8fb9c4d724

---
+++ tests/test_set.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+from testcase import TestCase
+
+class TestSset(TestCase):
+
+    def test_literal0(self):
+        self._test("""
+s = { 42 }
+print(s.include?(42))
+""", "42")
+
+    def test_literal10(self):
+        self._test("""
+s = { 42, 26 }
+print(s.include?(42))
+""", "true")
+
+    def test_literal20(self):
+        self._test("""
+s = { 42, 26 }
+print(s.include?(26))
+""", "true")
+
+    def test_literal30(self):
+        self._test("""
+s = { 42, 26 }
+print(s.include?(:foo))
+""", "false")
+
+    def test_new0(self):
+        self._test("""
+print(Set.new().size)
+""", "0")
+
+    def test_add0(self):
+        self._test("""
+s = Set.new()
+s.add(42)
+print(s.include?(42))
+""", "42")
+
+# vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
