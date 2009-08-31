3f96e37935b965ef092f1128a2748da4505092bf
SumiTomohiko <SumiTomohiko@neko-daisuki.ddo.jp>
2009-08-31 21:35:00 +0900

    Wrote how to use bugs plugin for ditz.

--- tools/bugs.rb
+++ tools/bugs.rb
@@ -1,3 +1,12 @@
+# Ditz bug plugin
+#
+# This plugin adds the "bugs" command to ditz. This command shows a list of
+# bugs.
+#
+# How to use
+#
+# 1. sudo cp tools/bugs.rb /usr/lib/ruby/gems/1.8/gems/ditz-0.5/lib/plugins
+# 2. add the "- bugs" entry to your project's .ditz-plugins.
 
 module Ditz
   class Operator
