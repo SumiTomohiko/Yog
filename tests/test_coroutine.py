e9c257ad7b3afaac64d5dd629a3f46c19417824d
SumiTomohiko <SumiTomohiko@neko-daisuki.ddo.jp>
2009-10-21 23:39:21 +0900

    Added a test for Coroutine#yield.

--- tests/test_coroutine.py
+++ tests/test_coroutine.py
@@ -12,4 +12,17 @@ end
 co.resume()
 """, "42")
 
+    def test_yield0(self):
+        self._test("""
+co = Coroutine.new() do
+  print(42)
+  Coroutine.yield()
+  print(44)
+end
+co.resume()
+print(43)
+co.resume()
+print(45)
+""", "42434445")
+
 # vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
