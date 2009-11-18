cf6ffcf9260626af29bc406305916a7bfa3c42a0
SumiTomohiko <SumiTomohiko@neko-daisuki.ddo.jp>
2009-11-18 14:35:00 +0900

    Added code to output xxx_wrap.c.

--- tools/swig/Source/Modules/yog.cxx
+++ tools/swig/Source/Modules/yog.cxx
@@ -1,17 +1,41 @@
 #include "swigmod.h"
 
 class YOG: public Language {
+protected:
+    File* f_begin;
+    File* f_runtime;
+
 public:
     YOG() {
     }
 
     virtual void main(int argc, char *argv[]) {
-#if 0
+        SWIG_library_directory("yog");
         SWIG_config_file("yog.swg");
-#endif
     }
 
     virtual int top(Node *n) {
+        String *outfile = Getattr(n, "outfile");
+        f_begin = NewFile(outfile, "w", SWIG_output_files());
+        if (!f_begin) {
+            FileErrorDisplay(outfile);
+            SWIG_exit(EXIT_FAILURE);
+        }
+        f_runtime = NewString("");
+
+        Swig_register_filebyname("begin", f_begin);
+        Swig_register_filebyname("runtime", f_runtime);
+
+        Swig_banner(f_begin);
+
+        Language::top(n);
+
+        Dump(f_runtime, f_begin);
+
+        Delete(f_runtime);
+        Close(f_begin);
+        Delete(f_begin);
+
         return SWIG_OK;
     }
 };
