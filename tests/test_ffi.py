c9e8c8dfa934b55fa93c50705b53836713d68a6f
SumiTomohiko <SumiTomohiko@neko-daisuki.ddo.jp>
2010-05-22 12:45:26 +0900

    Added a test for member access of Struct object.

--- tests/test_ffi.py
+++ tests/test_ffi.py
@@ -13,12 +13,20 @@ class TestFfi(TestCase):
         path = self.get_lib_path()
         self._test("load_lib(\"%(path)s\")" % locals())
 
-    def test_find_function0(self):
+    def test_find_func0(self):
         path = self.get_lib_path()
         self._test("""
 lib = load_lib(\"%(path)s\")
 f = lib.load_func(\"foo\")
 f()
 """ % locals(), "42")
+
+    def test_Struct0(self):
+        self._test("""
+st = StructClass.new(\"Foo\", [[\'int, \'bar]])
+o = st.new()
+o.bar = 42
+print(o.bar)
+""", "42")
 
 # vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
