1ae4f12bf107af0d487202c7760cab0db9a5a90f
SumiTomohiko <SumiTomohiko@neko-daisuki.ddo.jp>
2010-06-08 00:12:19 +0900

    Improved autogen.sh to detect lack of tools.

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-aclocal
-autoheader
-autoconf
-automake --add-missing
+run()
+{
+  "$@"
+  if [ $? != 0 ]; then
+    echo "$@ failed"
+    exit 1
+  fi
+}
+
+run aclocal
+run autoheader
+run autoconf
+run automake --add-missing
 (cd tools/swig && ./autogen.sh)
 
 # vim: tabstop=2 shiftwidth=2 expandtab softtabstop=2
