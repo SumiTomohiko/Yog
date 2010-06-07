f1c6933d96f3eb6a47eb66a5fd700666cbd75927
SumiTomohiko <SumiTomohiko@neko-daisuki.ddo.jp>
2010-06-08 01:24:06 +0900

    Added files for .deb package generated by dh_make --native --copyright bsd --single.

---
+++ debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the yog package
+#
+0 4	* * *	root	yog_maintenance
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(yog):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="yog" command="/usr/bin/yog"
