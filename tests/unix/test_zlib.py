05628732b334eb4406c8ec93922d56e351881a95
SumiTomohiko <SumiTomohiko@neko-daisuki.ddo.jp>
2010-09-11 01:58:45 +0900

    Changed zlib package to give encoding to string parameters.

--- tests/unix/test_zlib.py
+++ tests/unix/test_zlib.py
@@ -21,22 +21,22 @@ import zlib
 
     def test_compress_file0(self):
         data = []
-        for b in compress("foo"):
+        for b in compress("foo\x00"):
             data.append("\\x%02x" % (ord(b), ))
         self._test("""
 import zlib
 enable_gc_stress()
-print(zlib.compress(\"foo\".to_bin()))
+print(zlib.compress(\"foo\".to_bin(ENCODINGS[\"utf-8\"])))
 """, self.format_binary("".join(data)))
 
     def test_decompress_file0(self):
         data = []
-        for c in "foo":
+        for c in "foo\x00":
             data.append("\\x%02x" % (ord(c), ))
         self._test("""
 import zlib
 enable_gc_stress()
-print(zlib.decompress(zlib.compress(\"foo\".to_bin())))
+print(zlib.decompress(zlib.compress(\"foo\".to_bin(ENCODINGS[\"utf-8\"]))))
 """, self.format_binary("".join(data)))
 
 # vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
