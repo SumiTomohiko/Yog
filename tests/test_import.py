b976eef28ae65a47cf387532e7fda105debf47a4
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2012-10-16 03:51:12 +0900

    tests/test_import.py: Changed to work at any current directories (the new implementation calls chdir()).

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,22 +1,33 @@
 # -*- coding: utf-8 -*-
 
+from os import chdir, getcwd
+from os.path import dirname
 from re import match
+
 from testcase import TestCase
 
 class TestImport(TestCase):
 
+    def run_test(self, src, stdout="", stderr="", options=None):
+        old_dir = getcwd()
+        chdir(dirname(__file__))
+        try:
+            self._test(src=src, stdout=stdout, stderr=stderr, options=options)
+        finally:
+            chdir(old_dir)
+
     def test_import1(self):
-        self._test("import test_package1", "42\n")
+        self.run_test("import test_package1", "42\n")
 
     def test_import3(self):
-        self._test("""
+        self.run_test("""
 import test_package3
 
 test_package3.foo()""", """42
 """)
 
     def test_import10(self):
-        self._test("""
+        self.run_test("""
 import test_package3 as bar
 
 bar.foo()
@@ -24,42 +35,42 @@ bar.foo()
 """)
 
     def test_from0(self):
-        self._test("""
+        self.run_test("""
 from test_package3 import foo
 foo()
 """, """42
 """)
 
     def test_from10(self):
-        self._test("""
+        self.run_test("""
 from test_package3 import foo as bar
 bar()
 """, """42
 """)
 
     def test_from20(self):
-        self._test("""
+        self.run_test("""
 from test_package7 import foo, bar
 print(foo)
 """, "42")
 
     def test_from30(self):
-        self._test("""
+        self.run_test("""
 from test_package7 import foo, bar
 print(bar)
 """, "26")
 
     def test_from40(self):
-        self._test("""
+        self.run_test("""
 from test_package7 import foo as baz, bar
 print(baz)
 """, "42")
 
     def test_dl1(self):
-        self._test("import test_package4", "42\n")
+        self.run_test("import test_package4", "42\n")
 
     def test_dl2(self):
-        self._test("""
+        self.run_test("""
 import test_package5
 
 test_package5.foo()""", """42
@@ -73,7 +84,7 @@ test_package5.foo()""", """42
 ImportError: No package named "foo"
 """, stderr)
 
-        self._test("""
+        self.run_test("""
 import foo
 """, stderr=test_stderr)
 
@@ -85,23 +96,23 @@ import foo
 ImportError: Dynamic package does not define init function \(YogInit_test_package6\)
 """, stderr)
 
-        self._test("""
+        self.run_test("""
 import test_package6
 """, stderr=test_stderr)
 
     def test_directory0(self):
-        self._test("""
+        self.run_test("""
 import test_package8
 print(test_package8.foo)
 """, "42")
 
     def test_directory10(self):
-        self._test("""
+        self.run_test("""
 import test_package8.foo
 print(test_package8.foo.bar)
 """, "42")
 
     def test_lib_path0(self):
-        self._test("import foo", "42", options=["--lib-path=test_lib_path0"])
+        self.run_test("import foo", "42", options=["--lib-path=test_lib_path0"])
 
 # vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
