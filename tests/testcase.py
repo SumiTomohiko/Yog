ac24da19626a3b5f2a91a3e8e0f2f6915e7d071e
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2012-10-16 21:36:50 +0900

    Revert "tests/testcase.py (do): Removed the code handling encoding. It seems"
    This reverts commit 378a07af99126612b80c3277ed2188215f4de35f.

--- tests/testcase.py
+++ tests/testcase.py
@@ -125,12 +125,16 @@ class TestCase(object):
             out = self.read(stdout_path)
             print(out)
             if stderr is not None:
+                if encoding is not None:
+                    err = err.decode("UTF-8")
                 if callable(stderr):
                     stderr(err)
                 else:
                     assert stderr == err, "stderr must be %r, but actual is %r" % (stderr, err)
 
             if stdout is not None:
+                if encoding is not None:
+                    out = out.decode("UTF-8")
                 if callable(stdout):
                     stdout(out)
                 else:
