a9690845d67be48304c6d60b56a5c39aa18fb800
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2012-10-16 16:36:31 +0900

    Changed test tools to work on Python 2.7 and py.test 2.2.4.
    py.test on Python 3.x cannot handle methods defined by exec().

--- tests/testcase.py
+++ tests/testcase.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
+
 from re import search
 from os import close, environ, unlink
 from os.path import abspath, basename, dirname, exists, join, splitext
